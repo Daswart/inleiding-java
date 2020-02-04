bdd193967a5aa58e0c5d7828f00f758effc676d0
Daswart <60608985+Daswart@users.noreply.github.com>
2020-02-04 11:12:46 +0100

    Update HalloRocDev.java

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,6 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("Daan gaat leren programmeren!");
+        System.out.println("Daan gaat eindeljk leren programmeren!");
     }
 }
