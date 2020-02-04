f1b342cc49b2d0f9bfd644d5302014028c0ef412
Daswart <daswart@roc-dev.com>
2020-02-04 10:28:24 +0100

    tekst is gewijzigd

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,6 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("gewijzigde tekst!");
+        System.out.println("deze tekst is gewijzigd!");
     }
 }
