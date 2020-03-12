20ba6fd632552c0f1e432f5ae2d7f1f0c6bf181d
Daswart <daswart@roc-dev.com>
2020-03-12 14:20:22 +0100

    Klaar

---
+++ src/h10/Opdracht101.java
@@ -0,0 +1,61 @@
+package h10;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Opdracht101 extends Applet {
+    TextField tekstvak;
+    Label label;
+    String s, hogereScore, lageScore;
+    int getal;
+
+
+
+    public void init() {
+        label = new Label();
+        add(label);
+
+        tekstvak = new TextField("", 3);
+        add(tekstvak);
+        tekstvak.addActionListener(new TekstvakListener());
+
+        lageScore = "";
+        hogereScore = "";
+        s = "";
+
+
+
+
+
+    }
+
+    public void paint(Graphics g) {
+        g.drawString(hogereScore , 50 , 60);
+        g.drawString(lageScore, 50, 80);
+
+
+
+
+    }
+
+    class TekstvakListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            s = tekstvak.getText();
+//            getal = Integer.parseInt(s);
+            if (Integer.parseInt(s) >= getal) {
+                getal = Integer.parseInt(s);
+                hogereScore = Integer.toString(getal);
+                hogereScore = "De hoogste score is " + getal;
+                lageScore = "";
+
+            } else {
+                hogereScore = "De hoogste score is " + getal;
+                lageScore = "Voer een getal in die hoger is dan " + getal;
+
+            }
+            repaint();
+        }
+    }
+}
