77777f92d6e5095865ba4073eace5a99ff0683d1
Daswart <daswart@roc-dev.com>
2020-03-18 18:44:15 +0100

    H2 commentaar toegevoegd en nu klaar.

--- src/h02/Opdracht21.java
+++ src/h02/Opdracht21.java
@@ -5,10 +5,13 @@ import java.applet.*;
 import java.awt.*;
 
 public class Opdracht21 extends Applet {
+
+    // De achtergrond van de Applet is blauw.
     public void init() {
         setBackground(Color.blue);
     }
 
+    //Naam wordt afgedrukt in geel.
     public void paint(Graphics g) {
         g.setColor(Color.yellow);
         g.drawString("Daan Swart", 50, 60 );
--- src/h02/Opdracht22.java
+++ src/h02/Opdracht22.java
@@ -4,10 +4,14 @@ import java.applet.*;
 import java.awt.*;
 
 public class Opdracht22 extends Applet  {
+
+    //Achtergrond kleur Applet is wit.
     public void init() {
         setBackground(Color.white);
     }
 
+    //Roepnaam wordt in het blauw afgedrukt.
+    //Achternaam wordt in het rood afgedrukt.
     public void paint(Graphics g) {
         g.setColor(Color.blue);
         g.drawString("Daan", 50, 60 );
