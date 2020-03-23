ca075bf0c37819fb75870a499096c43dd1af327d
Daswart <daswart@roc-dev.com>
2020-03-23 16:16:53 +0100

    Opdracht 4 toegevoegd!

--- src/h10/Opdracht104.java
+++ src/h10/Opdracht104.java
@@ -1,43 +1,56 @@
 package h10;
 
 import java.awt.*;
-import java.applet.*;
+import java.applet.Applet;
 import java.awt.event.*;
 
 
-public class Opdracht103 extends Applet {
+public class Opdracht104 extends Applet {
     TextField tekstVakMaand;
-
+    TextField tekstVakJaar;
     Label labelMaand;
-
-    String s, tekst, dagInMaand;
-
-    int maand;
-
+    Label labelJaar;
     Font font;
-
+    String s, tekst, dagInMaand, dagenInFebruari;
+    int maand;
+    int jaartal;
+    boolean schrikkelJjaar = false;
 
     public void init() {
 
+        setSize(1200, 200);
+
         font = new Font("", Font.PLAIN,16);
 
-        labelMaand = new Label("Type het maandnummer en druk op enter");
-        add(labelMaand);
-        labelMaand.setFont(font);
-
         tekstVakMaand = new TextField("", 2);
+        labelMaand = new Label("Type het maandnummer en druk op Enter");
+        labelMaand.setFont(font);
+        add(labelMaand);
         tekstVakMaand.addActionListener(new TekstVakMaandListener());
         add(tekstVakMaand);
 
 
+
+        tekstVakJaar = new TextField("", 2);
+        labelJaar = new Label("Type het jaartal en druk op Enter");
+        add(labelJaar);
+        labelJaar.setFont(font);
+        tekstVakJaar.addActionListener(new TekstVakJaarListener());
+        add(tekstVakJaar);
+
+
+
+
         tekst = "";
         dagInMaand = "";
+        dagenInFebruari = "";
+
 
 
     }
 
     public void paint(Graphics g) {
-        g.setFont(new Font("", Font.PLAIN, 16));
+        g.setFont(new Font("Helvetica", Font.PLAIN, 16));
         g.drawString(tekst, 50, 60);
         g.drawString(dagInMaand, 50, 80);
     }
@@ -46,7 +59,6 @@ public class Opdracht103 extends Applet {
         public void actionPerformed(ActionEvent e) {
             s = tekstVakMaand.getText();
             maand = Integer.parseInt(s);
-
             switch (maand) {
                 case 1:
                     tekst = "januari";
@@ -54,8 +66,7 @@ public class Opdracht103 extends Applet {
                     break;
                 case 2:
                     tekst = "februari";
-                    dagInMaand = "er zitten 28 0f 29 dagen in " + tekst;
-
+                    dagInMaand = "voer een jaartal in om te zien hoeveel dagen februari heeft";
                     break;
                 case 3:
                     tekst = "maart";
@@ -104,11 +115,28 @@ public class Opdracht103 extends Applet {
 
                     break;
             }
-
             repaint();
         }
     }
 
+    class TekstVakJaarListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            s = tekstVakJaar.getText();
+            jaartal = Integer.parseInt(s);
+            schrikkelJjaar = (jaartal % 4 == 0 && !(jaartal % 100 == 0)) ||
+                    jaartal % 400 == 0;
+            if (maand == 2){
+
+
+                if (!schrikkelJjaar) {
+                    dagInMaand = "In het jaar "+ jaartal + " heeft februari 28 dagen" ;
+                } else {
+                    dagInMaand = "In het jaar " + jaartal + " heeft februari 29 dagen";
+                }
+            }
+            repaint();
+
+        }
+    }
 
 }
-
