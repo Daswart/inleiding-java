0fb53de182868a738607f89da925ccc2c62d441e
Daan Swart <dswart@roc-dev.com>
2020-04-21 15:26:02 +0200

    H07 Bijspijkercursus klaar

--- src/JavaBijspijkerCursus/Main7.java
+++ src/JavaBijspijkerCursus/Main7.java
@@ -2,13 +2,16 @@ package JavaBijspijkerCursus;
 
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main7 {
     public static void main(String[] args) {
+        Scanner mijnScanner = new Scanner(System.in);
+        int counter = 1;
 
         // OPDRACHT 7.1;
         System.out.println("Opdracht 7.1 \n");
-        System.out.println("Hier komen 100 worpen met een dobbelsteen \n");
+        System.out.println("Hier komen 100 worpen met een dobbelsteen: \n");
         //aanmaken array
         int[] dobbelsteenWorpen = new int[100];
 
@@ -18,7 +21,7 @@ public class Main7 {
             dobbelsteenWorpen[i] = random;
         }
         //uitprinten van de waardes.
-        int counter = 1;
+
         for (int worp : dobbelsteenWorpen) {
             System.out.print(worp + " ");
             if (counter % 10 == 0) {
@@ -30,10 +33,49 @@ public class Main7 {
                 System.out.println("\n");
             }
         }
+
         //OPDRACHT 7.2
         System.out.println("Opdracht 7.2 \n");
+        System.out.print("Geef me 9 integers: ");
+        int[] getallen = new int[9];
+        for (int i = 0; i < 9; i++) {
+            getallen[i] = mijnScanner.nextInt();
 
+        }
+        System.out.println("Daar komen ze, in omgekeerde volgorde: ");
+        for (int i = 8; i >= 0; i--) {
+            System.out.print(getallen[i] + " ");
+
+        }
+        System.out.println("\n\n");
+
+        //OPDRACHT 7.3
+        System.out.println("Opdracht 7.3 \n");
+        System.out.println("Geef me nu 10 doubles, dan bereken ik het gemiddelde en tel ik hoeveel cijfers hoger zijn: ");
+        double[] userInput = new double[10];
+        for (int i = 0; i < 10; i++) {
+            userInput[i] = mijnScanner.nextDouble();
+        }
+        double optelsom = 0;
+        for (double waarde : userInput) {
+            optelsom = optelsom + waarde;
+        }
+        double gemiddelde = optelsom / 10;
+        System.out.println("Het gemiddelde is " + gemiddelde);
+
+        counter = 0;
+        for (double waarde : userInput) {
+            if (waarde > gemiddelde) {
+                counter++;
+
+            }
+
+        }
+        System.out.println("Het aantaal doubles boven het gemiddelde is: " + counter);
     }
 
+
 }
+
+
 
