dfe5e3ba340381d1c2fe3733bf9492d0c95f4e17
Daan Swart <dswart@roc-dev.com>
2020-05-07 15:11:19 +0200

    Alle methodes werken, behalve de uitknop.

--- src/JavaBijspijkerCursus/Television.java
+++ src/JavaBijspijkerCursus/Television.java
@@ -1,30 +1,66 @@
 package JavaBijspijkerCursus;
 
+import java.nio.channels.Channel;
 import java.util.Scanner;
 
 public class Television {
     private int channel = 1;
     private int volumeLevel = 1;
     private boolean power = false;
+    static Scanner myScanner = new Scanner(System.in);
+    static int userChoice = 0;
 
 
     public static void main(String[] args) {
 
+
         Television myFirstTelevision = new Television();
 
+        myFirstTelevision.turnOff();
+
+        while (myFirstTelevision.power = true) {
+            myFirstTelevision.printMenu();
+            System.out.print("\nMaak uw keuze: ");
+            userChoice = myScanner.nextInt();
+            switch (userChoice) {
+                case 1:
+                    myFirstTelevision.turnOn();
+                    break;
+                case 2:
+                    myFirstTelevision.turnOff();
+                case 3:
+                    myFirstTelevision.channelUp();
+                    break;
+                case 4:
+                    myFirstTelevision.channelDown();
+                    break;
+                case 5:
+                    myFirstTelevision.volumeUp();
+                    break;
+                case 6:
+                    myFirstTelevision.volumeDown();
+                    break;
+                case 7:
+                    System.exit(0);
+
+
+            }
+
+
+        }
 
 
     }
 
     public void turnOn() {
         power = true;
-        System.out.println("Tv is aan");
+        System.out.println("\nDe TV staat nu aan.");
 
     }
 
     public void turnOff() {
         power = false;
-        System.out.println("tv is uit");
+        System.out.println("TV is off");
     }
 
     public void setChannel(int newChannel) {
@@ -37,7 +73,7 @@ public class Television {
     public void setVolume(int newVolumelevel) {
         if (power && newVolumelevel >= 1 && newVolumelevel <= 7) {
             volumeLevel = newVolumelevel;
-            System.out.println("het volume staat nu op" + volumeLevel);
+            System.out.println("Volumelevel = " + volumeLevel);
         }
 
     }
@@ -45,30 +81,51 @@ public class Television {
     public void channelUp() {
         if (power && channel < 120) {
             channel++;
+            System.out.println("De TV Staat nu op " + channel);
+
         }
     }
 
     public void channelDown() {
         if (power && channel > 1) {
             channel--;
+            System.out.println("Channel = " + channel);
+        } else {
+            System.out.println("Channel = " + channel + "." + " Can't go lower!");
         }
     }
 
     public void volumeUp() {
         if (power && volumeLevel < 120) {
             volumeLevel++;
+            System.out.println("Volume = " + volumeLevel);
+
         }
 
 
     }
 
     public void volumeDown() {
-        if(power && volumeLevel > 0){
+        if (power && volumeLevel > 0) {
             volumeLevel--;
+            System.out.println("Volume = " + volumeLevel);
+        } else {
+            System.out.println("Volumelevel =");
         }
 
     }
+
+    public void printMenu() {
+        System.out.println("\n1. ON");
+        System.out.println("2. OFF");
+        System.out.println("3. CHANNEL UP");
+        System.out.println("4. CHANNEL DOWN");
+        System.out.println("5. VOLUME UP");
+        System.out.println("6. VOLUME DOWN");
+        System.out.println("7. EXIT");
+    }
 }
+
 
 //MOGELIJK NOG TE GEBRUIKEN CODE (DAAN LEZEN)
 /*Scanner myScanner = new Scanner(System.in);
