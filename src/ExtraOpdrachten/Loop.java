32a91e9d064928b0e7ae73991179acdb8601d827
Daan Swart <dswart@roc-dev.com>
2020-06-09 17:41:56 +0200

    Loop toegevoegd

---
+++ src/ExtraOpdrachten/Loop.java
@@ -0,0 +1,17 @@
+package ExtraOpdrachten;
+
+public class Loop {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++ ){
+            //System.out.println("dit is regel " + i );
+
+            if(i == 5){
+                System.out.println("We zijn nu op de helft");
+            }
+            else{
+                System.out.println("dit is regel " + i);
+            }
+        }
+
+    }
+}
